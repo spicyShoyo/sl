6bd9f663635cd2d838af585764abe712efb0ce4b
Liu <zliu80@illinois.edu>
2016-11-04 16:02:43 -0500

    l

---
+++ StepThroughPortal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StepThroughPortal : MonoBehaviour {
+    public GameObject otherPortal;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
+        if (other.tag == "Player")
+        {
+            other.transform.position = otherPortal.transform.position + otherPortal.transform.forward*5;
+        }
+    }
+}
