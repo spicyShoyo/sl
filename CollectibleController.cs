5116d76c3b8551975a8e02f6159183aeb282c621
spicyShoyo <lzhengchao18@outlook.com>
2016-11-15 12:38:58 -0600

    Add files via upload

---
+++ CollectibleController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleController : MonoBehaviour {
+    public GameObject cube;
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cube.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if(Vector3.Distance(player.transform.position, gameObject.transform.position) < 3)
+        {
+            collected();
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            collected();
+        }
+    }
+
+    private void collected()
+    {
+        Destroy(gameObject);
+    }
+}
