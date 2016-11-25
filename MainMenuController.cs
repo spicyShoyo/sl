4e1b1f3221f914e78daeac69ff2410365499de95
Liu <zliu80@illinois.edu>
2016-11-25 15:05:40 -0600

    11/25

--- MainMenuController.cs
+++ MainMenuController.cs
@@ -3,18 +3,25 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour {
+public class MainMenuController : MonoBehaviour
+{
     public Text mainMenuText;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown("joystick button 0"))
         {
             SceneManager.LoadScene("Falling", LoadSceneMode.Single);
+        }
+        if (Input.GetKeyDown("joystick button 2"))
+        {
+            SceneManager.LoadScene("City", LoadSceneMode.Single);
         }
     }
 }
